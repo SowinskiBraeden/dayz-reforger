e73ba298f4b24c479fa46878c5d77eaa39ddc2e0
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-04-17 15:34:28 -0700

    debug combat log

--- util/AdminLogsHandler.js
+++ util/AdminLogsHandler.js
@@ -35,7 +35,8 @@ module.exports = {
 
     // If diff is greater than 5 minutes, not a combat log
     // or if death after last combat and death was before logout event
-    if (diff > 5 || (data.lastDamageDate < data.lastDeathDate && data.lastDeathDate < newDt)) return;
+    if (data.lastDamageDate < data.lastDeathDate && data.lastDeathDate < newDt1) return;
+    if (diff > 5) return;
 
     let guild = await client.GetGuild(guildId);
     if (!client.exists(guild.connectionLogsChannel)) return;
