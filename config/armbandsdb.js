72e842e263fccc3301ee2ae6e7c55b76db328cb8
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-08-30 18:55:04 -0700

    update/armbands-list

--- config/armbandsdb.js
+++ config/armbandsdb.js
@@ -1,152 +1,164 @@
 module.exports = {
   Armbands: [
     {
-      name: "Armband (Black)",
+      name: "Black",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/8/82/ArmbandBlack.png/revision/latest?cb=20161127174754"
     },
     {
-      name: "Armband (Blue)",
+      name: "Blue",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/b/bd/ArmbandBlue.png/revision/latest?cb=20161127174803"
     },
     {
-      name: "Armband (Green)",
+      name: "Green",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/ce/ArmbandGreen.png/revision/latest?cb=20161127174812"
     },
     {
-      name: "Armband (Orange)",
+      name: "Orange",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/e6/ArmbandOrange.png/revision/latest?cb=20161127174846"
     },
     {
-      name: "Armband (Pink)",
+      name: "Pink",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/f/f7/ArmbandPink.png/revision/latest?cb=20161127174854"
     },
     {
-      name: "Armband (Red)",
+      name: "Red",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/1/14/Armband.png/revision/latest?cb=20161127174901"
     },
     {
-      name: "Armband (Yellow)",
+      name: "Yellow",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/8/81/ArmbandYellow.png/revision/latest?cb=20161127174918"
     },
     {
-      name: "Armband (White)",
+      name: "White",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/c7/Armband_White.png/revision/latest?cb=20161127174926"
     },
     {
-      name: "Armband (Altis)",
+      name: "Altis",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/ee/Flag_alti_co.png/revision/latest?cb=20200820222622"
     },
     {
-      name: "Armband (APA)",
+      name: "Asiain Pacific Alliance (APA)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/c2/Flag_apa_co.png/revision/latest?cb=20200820222623"
     },
     {
-      name: "Armband (Bear)",
+      name: "Bear",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/e1/Flag_bear_co.png/revision/latest?cb=20200820222626"
     },
     {
-      name: "Armband (Bohemia)",
+      name: "Bohemia Interactive",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/ee/Flag_bi_co.png/revision/latest?cb=20200820222627"
     },
     {
-      name: "Armband (Brain)",
+      name: "Brain",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/7/7d/Flag_brain_co.png/revision/latest?cb=20200820222628"
     },
     {
-      name: "Armband (CDF)",
+      name: "Chernarussian Defence Forces (CDF)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/d/d6/Flag_cdf_co.png/revision/latest?cb=20200820222629"
     },
     {
-      name: "Armband (CHED.)",
+      name: "Chedaki (CHED)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/9/96/Flag_ched_co.png/revision/latest?cb=20200820222630"
     },
     {
-      name: "Armband (CHEL)",
+      name: "CHEL",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/9/98/Flag_chel_co.png/revision/latest?cb=20200820222631"
     },
     {
-      name: "Armband (Chernarus)",
+      name: "Chernarus",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/ef/Flag_chern_co.png/revision/latest?cb=20200820222632"
     },
     {
-      name: "Armband (CMC)",
+      name: "Chernarus Mining Corporation (CMC)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/d/da/Flag_cmc_co.png/revision/latest?cb=20200820222634"
     },
     {
-      name: "Armband (Rooster)",
+      name: "Rooster",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/4/44/Flag_cock_co.png/revision/latest?cb=20200820222635"
     },
     {
-      name: "Armband (DayZ)",
+      name: "DayZ",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/b/b2/Flag_dayz_co.png/revision/latest?cb=20200820222636"
     },
     {
-      name: "Armband (DROS)",
+      name: "North Sahrani (DROS)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/2/24/Flag_dros_co.png/revision/latest?cb=20200820222637"
     },
     {
-      name: "Armband (Fawn)",
+      name: "Fawn",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/d/d2/Flag_fawn_co.png/revision/latest/scale-to-width-down/1000?cb=20200820222639"
     },
     {
-      name: "Armband (Pirates)",
+      name: "Pirates",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/a/ab/Flag_jolly_co.png/revision/latest?cb=20200820222643"
     },
     {
-      name: "Armband (Cannibals)",
+      name: "Cannibals",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/4/42/Flag_jolly_c_co.png/revision/latest?cb=20200820222641"
     },
     {
-      name: "Armband (KOS)",
+      name: "North Sahrani (KOS)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/a/a1/Flag_kos_co.png/revision/latest?cb=20200820222644"
     },
     {
-      name: "Armband (LDF)",
+      name: "Livonia Army (LDF)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/c1/Flag_ldf_co.png/revision/latest?cb=20200820222645"
     },
     {
-      name: "Armband (Livonia)",
+      name: "Livonia",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/e6/Flag_livo_co.png/revision/latest?cb=20200820222647"
     },
     {
-      name: "Armband (NAPA)",
+      name: "NAPA",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/e4/Flag_napa_co.png/revision/latest?cb=20200820222648"
     },
     {
-      name: "Armband (Police)",
+      name: "Livonia Police",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/6/63/Flag_police_co.png/revision/latest?cb=20200820222649"
     },
     {
-      name: "Armband (TEC)",
+      name: "TEC",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/e/ea/Flag_tec_co.png/revision/latest?cb=20200820222650"
     },
     {
-      name: "Armband (UEC)",
+      name: "United Earth Coalition (UEC)",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/ca/Flag_uec_co.png/revision/latest?cb=20200820222651"
     },
     {
-      name: "Armband (Wolf)",
+      name: "Wolf",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/b/b2/Flag_wolf_co.png/revision/latest?cb=20200820222653"
     },
     {
-      name: "Armband (Zenit)",
+      name: "Zenit Radio Station",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/0/05/Flag_zenit_co.png/revision/latest?cb=20200820222654"
     },
     {
-      name: "Armband (Z-Hunters)",
+      name: "Zombie Hunters",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/9/97/Flag_zhunters_co.png/revision/latest?cb=20200820222621"
     },
     {
-      name: "Armband (RSTA)",
+      name: "RSTA",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/2/20/Flag_rsta_co.png/revision/latest/scale-to-width-down/1000?cb=20210216191221"
     },
     {
-      name: "Armband (Refuge)",
+      name: "Refuge",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/8/8e/Flag_refuge_co.png/revision/latest/scale-to-width-down/1000?cb=20210216191205"
     },
     {
-      name: "Armband (Snake)",
+      name: "Snake",
       url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/5/54/Flag_snake_co.png/revision/latest/scale-to-width-down/1000?cb=20210216191234"
+    },
+    {
+      name: "Zagorky",
+      url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/7/75/Flag_zagorky_co.png/revision/latest/scale-to-width-down/1000?cb=20230619164704"
+    },
+    {
+      name: "Crook",
+      url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/c8/Flag_crook_co.png/revision/latest/scale-to-width-down/1000?cb=20230619164705"
+    },
+    {
+      name: "Rex",
+      url: "https://static.wikia.nocookie.net/dayz_gamepedia/images/c/c5/Flag_rex_co.png/revision/latest/scale-to-width-down/1000?cb=20230619164706"
     },
   ]
 }
