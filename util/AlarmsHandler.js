d3b211a30a02b77bd7a80dc0e91a840bff60b85e
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-09-03 17:09:48 -0700

    change/make certain functions private

--- util/AlarmsHandler.js
+++ util/AlarmsHandler.js
@@ -1,10 +1,49 @@
 const { BanPlayer, UnbanPlayer } = require('./NitradoAPI');
 const { EmbedBuilder } = require('discord.js');
 
+// Private functions (only called locally)
+
+const ExpireEvent = async(client, guild, e) => {
+  let hasMR = (guild.memberRole != "");
+  const channel = client.GetChannel(e.channel);
+  if (client.exists(e.channel)) channel.send({ embeds: [new EmbedBuilder().setColor(client.config.colors.Default).setDescription(`${hasMR ? `<@&${guild.memberRole}>\n`:''}**The ${e.name} Event has ended!**`)] });
+
+  client.dbo.collection("guilds").updateOne({ "server.serverID": guild.serverID }, {
+    $pull: {
+      "server.events": e
+    }
+  }, function(err, res) {
+    if (err) return client.sendError(client.GetChannel(guild.adminLogsChannel), err);
+  });
+}
+
+const HandlePlayerTrackEvent = async (client, guild, e) => {
+  let player = guild.playerstats.find(stat => stat.gamertag == e.gamertag );
+  let hasMR = (guild.memberRole != "")
+
+  let newDt = await client.getDateEST(player.time);
+  let unixTime = Math.floor(newDt.getTime()/1000);
+
+  const trackEvent = new EmbedBuilder()
+    .setColor(client.config.Colors.Default)
+    .setDescription(`**${e.name} Event**\n${e.gamertag} was located at **[${player.pos[0]}, ${player.pos[1]}](https://www.izurvive.com/chernarusplussatmap/#location=${player.pos[0]};${player.pos[1]})** at <t:${unixTime}>`);
+
+  if (!client.exists(e.channel)) return ExpireEvent(client, guild, e); // Expire event since it has invalid channel.
+  const channel = client.GetChannel(e.channel);
+  channel.send({ embeds: [trackEvent], content: `${hasMR ? `\n<@&${guild.memberRole}>`:'@here'}` });
+
+  let now = new Date();
+  let diff = ((now - e.creationDate) / 1000) / 60;
+  let minutesBetweenDates = Math.abs(Math.round(diff));
+
+  if (minutesBetweenDates >= e.time) ExpireEvent(client, guild, e);
+}
+
+// Public functions (called externally)
+
 module.exports = {
 
   HandleAlarmsAndUAVs: async (client, guildId, data) => {
-
     let guild = await client.GetGuild(guildId);
 
     for (let i = 0; i < guild.alarms.length; i++) {
@@ -125,50 +164,13 @@ module.exports = {
     }
   },
 
-  ExpireEvent: async(client, guild, e) => {
-    let hasMR = (guild.memberRole != "");
-    const channel = client.GetChannel(e.channel);
-    if (client.exists(e.channel)) channel.send({ embeds: [new EmbedBuilder().setColor(client.config.colors.Default).setDescription(`${hasMR ? `<@&${guild.memberRole}>\n`:''}**The ${e.name} Event has ended!**`)] });
-
-    client.dbo.collection("guilds").updateOne({ "server.serverID": guild.serverID }, {
-      $pull: {
-        "server.events": e
-      }
-    }, function(err, res) {
-      if (err) return client.sendError(client.GetChannel(guild.adminLogsChannel), err);
-    });
-  },
-
-  HandlePlayerTrackEvent: async (client, guild, e) => {
-
-    let player = guild.playerstats.find(stat => stat.gamertag == e.gamertag );
-    let hasMR = (guild.memberRole != "")
-
-    let newDt = await client.getDateEST(player.time);
-    let unixTime = Math.floor(newDt.getTime()/1000);
-
-    const trackEvent = new EmbedBuilder()
-      .setColor(client.config.Colors.Default)
-      .setDescription(`**${e.name} Event**\n${e.gamertag} was located at **[${player.pos[0]}, ${player.pos[1]}](https://www.izurvive.com/chernarusplussatmap/#location=${player.pos[0]};${player.pos[1]})** at <t:${unixTime}>`);
-
-    if (!client.exists(e.channel)) return module.exports.ExpireEvent(client, guild, e); // Expire event since it has invalid channel.
-    const channel = client.GetChannel(e.channel);
-    channel.send({ embeds: [trackEvent], content: `${hasMR ? `\n<@&${guild.memberRole}>`:'@here'}` });
-  
-    let now = new Date();
-    let diff = ((now - e.creationDate) / 1000) / 60;
-    let minutesBetweenDates = Math.abs(Math.round(diff));
-
-    if (minutesBetweenDates >= e.time) module.exports.ExpireEvent(client, guild, e);
-  },
-
   HandleEvents: async (client, guildId) => {
 
     let guild = await client.GetGuild(guildId);
 
     for (let i = 0; i < guild.events.length; i++) {
       let event = guild.events[i];
-      if (event.type == 'player-track') module.exports.HandlePlayerTrackEvent(client, guild, event);
+      if (event.type == 'player-track') HandlePlayerTrackEvent(client, guild, event);
     }
   },
 }
