de34ce952910b1a33988a6a64f959dfaaa4430c3
IrPgFKS0 <2511445@gmail.com>
2023-09-06 14:44:42 -0700

    Improved logging to file on uncaught exceptions

--- index.js
+++ index.js
@@ -2,5 +2,29 @@ const DayzR = require('./structures/DayzRBot');
 const config = require('./config/config');
 const { GatewayIntentBits } = require('discord.js');
 
+const path = require("path");
+const fs = require('fs');
+
 let client = new DayzR({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] }, config);
 client.build()
+
+// Log all uncaught exceptions before killing process.
+process.on('uncaughtException', async (error) => {
+    let d = new Date();
+    // Asynchronously write the error message to a log file using Promises
+    await new Promise((resolve, reject) => {
+        fs.appendFile(path.join(__dirname, "./logs/Logs.log"), 
+          `{"level":"error","message":"${d.getHours()}:${d.getMinutes()} - ${d.getMonth()+1}:${d.getDate()}:${d.getFullYear()} | uncaughtException: ${error.stack}"}`, (logErr) => {
+            if (logErr) {
+                console.error('Error writing uncaughtException to log file:', logErr);
+                reject(logErr);
+                process.exit()
+            } else {
+                resolve();
+            }
+        });
+    });
+
+    // Now, you can re-throw the error
+    process.exit()
+});
