66660657da9fef7d5bc05b5e90528310f62d3667
SowinskiBraeden <sowinskibraeden@gmail.com>
2024-01-27 11:31:45 -0800

    fix/vector compass directions

--- util/Vector.js
+++ util/Vector.js
@@ -4,7 +4,7 @@ module.exports = {
     let distance = parseFloat(Math.sqrt(Math.pow(delta[0], 2) + Math.pow(delta[1], 2)).toFixed(0));
     let thetat = Math.round(Math.atan2(delta[0], delta[1]) / Math.PI * 180);
     let theta = (thetat < 0) ? (360 + thetat) : thetat;
-    let compass = ["S", "SW", "W", "NW", "N", "N by NE", "NE", "E", "SE", "S"];
+    let compass = ["S", "SW", "W", "NW", "N", "NE", "E", "SE", "S"];
     let dir = compass[Math.round(theta / 45)];
   
     return {distance, theta, dir}
